4a15ce0054f38756886f187a289609a0dc48ca60
mattisafur <46404761+mattisafur@users.noreply.github.com>
2026-05-29 12:29:33 +0200

    updated code to use new data structure

--- app.py
+++ app.py
@@ -28,36 +28,136 @@ async def root():
     # mongo_client.close()
 
     documents = [
-        {"_id": "doc_2", "lng": 4.367875, "lat": 52.001839, "powerOutput": 97.47},
-        {"_id": "doc_3", "lng": 4.825745, "lat": 52.761230, "powerOutput": 30.1},
-        {"_id": "doc_4", "lng": 6.578064, "lat": 53.214257, "powerOutput": 1.44},
-        {"_id": "doc_5", "lng": 6.163330, "lat": 52.226117, "powerOutput": 87.28},
-        {"_id": "doc_6", "lng": 5.313860, "lat": 51.487193, "powerOutput": 81.56},
-        {"_id": "doc_7", "lng": 5.822754, "lat": 51.284253, "powerOutput": 12.13},
-        {"_id": "doc_8", "lng": 5.877686, "lat": 50.878777, "powerOutput": 48.3},
-        {"_id": "doc_9", "lng": 5.350342, "lat": 52.422523, "powerOutput": 68.58},
-        {"_id": "doc_10", "lng": 5.581055, "lat": 53.179704, "powerOutput": 62.92},
-        {"_id": "doc_11", "lng": 4.443970, "lat": 51.589016, "powerOutput": 55.06},
-        {"_id": "doc_12", "lng": 3.817749, "lat": 51.721924, "powerOutput": 15.13},
-        {"_id": "doc_13", "lng": 3.641968, "lat": 51.558290, "powerOutput": 94.21},
-        {"_id": "doc_14", "lng": 3.573687, "lat": 51.478496, "powerOutput": 42.84},
-        {"_id": "doc_15", "lng": 3.903580, "lat": 51.269648, "powerOutput": 74.65},
-        {"_id": "doc_16", "lng": 5.595629, "lat": 51.716298, "powerOutput": 35.96},
-        {"_id": "doc_17", "lng": 5.866699, "lat": 52.331982, "powerOutput": 88.56},
-        {"_id": "doc_18", "lng": 6.978001, "lat": 53.124712, "powerOutput": 91.09},
-        {"_id": "doc_19", "lng": 6.652222, "lat": 52.321911, "powerOutput": 68.61},
-        {"_id": "doc_20", "lng": 4.784546, "lat": 53.077528, "powerOutput": 5.23},
-        {"_id": "doc_21", "lng": 6.778564, "lat": 53.452896, "powerOutput": 38.12},
-        {"_id": "doc_22", "lng": 6.696167, "lat": 51.978959, "powerOutput": 10.64},
-        {"_id": "doc_23", "lng": 7.014771, "lat": 52.244620, "powerOutput": 20.56},
-        {"_id": "doc_24", "lng": 5.336716, "lat": 51.818473, "powerOutput": 39.07},
-        {"_id": "doc_25", "lng": 5.314636, "lat": 51.920556, "powerOutput": 81.27},
-        {"_id": "doc_26", "lng": 4.746094, "lat": 52.217704, "powerOutput": 19.63},
+        {
+            "_id": "doc_2",
+            "location": {"type": "Point", "coordinates": [4.367875, 52.001839]},
+            "expectedPowerOutput": 97.47,
+        },
+        {
+            "_id": "doc_3",
+            "location": {"type": "Point", "coordinates": [4.825745, 52.761230]},
+            "expectedPowerOutput": 30.1,
+        },
+        {
+            "_id": "doc_4",
+            "location": {"type": "Point", "coordinates": [6.578064, 53.214257]},
+            "expectedPowerOutput": 1.44,
+        },
+        {
+            "_id": "doc_5",
+            "location": {"type": "Point", "coordinates": [6.163330, 52.226117]},
+            "expectedPowerOutput": 87.28,
+        },
+        {
+            "_id": "doc_6",
+            "location": {"type": "Point", "coordinates": [5.313860, 51.487193]},
+            "expectedPowerOutput": 81.56,
+        },
+        {
+            "_id": "doc_7",
+            "location": {"type": "Point", "coordinates": [5.822754, 51.284253]},
+            "expectedPowerOutput": 12.13,
+        },
+        {
+            "_id": "doc_8",
+            "location": {"type": "Point", "coordinates": [5.877686, 50.878777]},
+            "expectedPowerOutput": 48.3,
+        },
+        {
+            "_id": "doc_9",
+            "location": {"type": "Point", "coordinates": [5.350342, 52.422523]},
+            "expectedPowerOutput": 68.58,
+        },
+        {
+            "_id": "doc_10",
+            "location": {"type": "Point", "coordinates": [5.581055, 53.179704]},
+            "expectedPowerOutput": 62.92,
+        },
+        {
+            "_id": "doc_11",
+            "location": {"type": "Point", "coordinates": [4.443970, 51.589016]},
+            "expectedPowerOutput": 55.06,
+        },
+        {
+            "_id": "doc_12",
+            "location": {"type": "Point", "coordinates": [3.817749, 51.721924]},
+            "expectedPowerOutput": 15.13,
+        },
+        {
+            "_id": "doc_13",
+            "location": {"type": "Point", "coordinates": [3.641968, 51.558290]},
+            "expectedPowerOutput": 94.21,
+        },
+        {
+            "_id": "doc_14",
+            "location": {"type": "Point", "coordinates": [3.573687, 51.478496]},
+            "expectedPowerOutput": 42.84,
+        },
+        {
+            "_id": "doc_15",
+            "location": {"type": "Point", "coordinates": [3.903580, 51.269648]},
+            "expectedPowerOutput": 74.65,
+        },
+        {
+            "_id": "doc_16",
+            "location": {"type": "Point", "coordinates": [5.595629, 51.716298]},
+            "expectedPowerOutput": 35.96,
+        },
+        {
+            "_id": "doc_17",
+            "location": {"type": "Point", "coordinates": [5.866699, 52.331982]},
+            "expectedPowerOutput": 88.56,
+        },
+        {
+            "_id": "doc_18",
+            "location": {"type": "Point", "coordinates": [6.978001, 53.124712]},
+            "expectedPowerOutput": 91.09,
+        },
+        {
+            "_id": "doc_19",
+            "location": {"type": "Point", "coordinates": [6.652222, 52.321911]},
+            "expectedPowerOutput": 68.61,
+        },
+        {
+            "_id": "doc_20",
+            "location": {"type": "Point", "coordinates": [4.784546, 53.077528]},
+            "expectedPowerOutput": 5.23,
+        },
+        {
+            "_id": "doc_21",
+            "location": {"type": "Point", "coordinates": [6.778564, 53.452896]},
+            "expectedPowerOutput": 38.12,
+        },
+        {
+            "_id": "doc_22",
+            "location": {"type": "Point", "coordinates": [6.696167, 51.978959]},
+            "expectedPowerOutput": 10.64,
+        },
+        {
+            "_id": "doc_23",
+            "location": {"type": "Point", "coordinates": [7.014771, 52.244620]},
+            "expectedPowerOutput": 20.56,
+        },
+        {
+            "_id": "doc_24",
+            "location": {"type": "Point", "coordinates": [5.336716, 51.818473]},
+            "expectedPowerOutput": 39.07,
+        },
+        {
+            "_id": "doc_25",
+            "location": {"type": "Point", "coordinates": [5.314636, 51.920556]},
+            "expectedPowerOutput": 81.27,
+        },
+        {
+            "_id": "doc_26",
+            "location": {"type": "Point", "coordinates": [4.746094, 52.217704]},
+            "expectedPowerOutput": 19.63,
+        },
     ]
 
-    lngs = np.array([document["lng"] for document in documents])
-    lats = np.array([document["lat"] for document in documents])
-    power_output = np.array([document["powerOutput"] for document in documents])
+    lngs = np.array([document["location"]["coordinates"][0] for document in documents])
+    lats = np.array([document["location"]["coordinates"][1] for document in documents])
+    power_output = np.array([document["expectedPowerOutput"] for document in documents])
 
     projection_transformer = pyproj.Transformer.from_crs(
         "EPSG:4326", "EPSG:3857", always_xy=True
