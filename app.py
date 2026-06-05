c196587a270cc26794de0c94ce33f57f29b9761a
mattisafur <46404761+mattisafur@users.noreply.github.com>
2026-06-05 21:30:12 +0200

    rewrote entire api
    - implemented pydantic classes for type chacking
    - added "nearest" and "prediction-interpolated" endpoints
    - added geojson-pydantic library for prefedines geojson classes

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import io
 import os
-from typing import Literal
-import dotenv
+from contextlib import asynccontextmanager
 
 import contextily
-import matplotlib.pyplot as plt
+import dotenv
 import numpy as np
-import pymongo
 import pyproj
-from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, Response
+from geojson_pydantic import Point
+from matplotlib import pyplot as plt
 from pydantic import BaseModel, Field
+from pymongo import AsyncMongoClient
 
 FIGURE_SIZE: tuple[int, int] = (12, 10)
 FIGURE_DPI: int = 150
@@ -20,33 +20,64 @@ MONGO_URI = os.environ["MONGO_URI"]
 MONGO_DB = os.environ["MONGO_DB"]
 MONGO_COLLECTION = os.environ["MONGO_COLLECTION"]
 
-app = FastAPI()
+
+class Mongo:
+    client: AsyncMongoClient
 
 
-class Location(BaseModel):
-    type: Literal["Point"]
-    coordinates: tuple[float, float]
+mongo = Mongo()
 
 
-class Document(BaseModel):
-    location: Location
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    mongo.client = AsyncMongoClient(MONGO_URI)
+
+    yield
+
+    await mongo.client.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+class Prediction(BaseModel):
+    location: Point
     expected_power_output: float = Field(alias="expectedPowerOutput")
 
 
-@app.get("/")
-async def root() -> StreamingResponse:
-    mongo_client = pymongo.MongoClient(MONGO_URI)
-    mongo_db = mongo_client[MONGO_DB]
-    mongo_collection = mongo_db[MONGO_COLLECTION]
-    documents = list(mongo_collection.find())
-    mongo_client.close()
+class DiagramResponse(Response):
+    media_type = "image/svg+xml"
 
-    parsed_docs = [Document.model_validate(document) for document in documents]
 
-    lngs = np.array([document.location.coordinates[0] for document in parsed_docs])
-    lats = np.array([document.location.coordinates[1] for document in parsed_docs])
-    power_output = np.array(
-        [document.expected_power_output for document in parsed_docs]
+async def get_nearest_prediction(location: Point) -> Prediction:
+    result = await mongo.client[MONGO_DB][MONGO_COLLECTION].find_one(
+        {"location": {"$near": {"$geometry": location.model_dump()}}}
+    )
+    return Prediction.model_validate(result)
+
+
+async def get_all_predictions() -> list[Prediction]:
+    results = await mongo.client[MONGO_DB][MONGO_COLLECTION].find().to_list()
+    return [Prediction.model_validate(result) for result in results]
+
+
+@app.get("/nearest")
+async def nearest_prediction(location: Point) -> Prediction:
+    return await get_nearest_prediction(location)
+
+
+@app.get("/prediction-interpolated")
+async def interpolated_prediction(location: Point) -> Prediction: ...
+
+
+@app.get("/map")
+async def map() -> DiagramResponse:
+    predictions = await get_all_predictions()
+
+    lngs = np.array([prediction.location.coordinates[0] for prediction in predictions])
+    lats = np.array([prediction.location.coordinates[1] for prediction in predictions])
+    expected_power_outputs = np.array(
+        [prediction.expected_power_output for prediction in predictions]
     )
 
     projection_transformer = pyproj.Transformer.from_crs(
@@ -59,7 +90,7 @@ async def root() -> StreamingResponse:
     )
     axes.set_axis_off()
 
-    scatter = axes.scatter(x_coordinates, y_coordinates, c=power_output)
+    scatter = axes.scatter(x_coordinates, y_coordinates, c=expected_power_outputs)
 
     contextily.add_basemap(axes)
 
@@ -71,4 +102,4 @@ async def root() -> StreamingResponse:
     plt.close(figure)
 
     output_buffer.seek(0)
-    return StreamingResponse(output_buffer, media_type="image/svg+xml")
+    return DiagramResponse(output_buffer.getbuffer())
